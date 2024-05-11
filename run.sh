1429f6a237a518a7d92a66e551ae11d2d8f22990
Terry Zhuo <terryzhuo25@gmail.com>
2024-05-12 04:19:43 +0800

    update: uncomment the sample running

--- run.sh
+++ run.sh
@@ -1,39 +1,42 @@
 #Qwen/CodeQwen1.5-7B-Chat
 #gpt-3.5-turbo-0125
-#deepseek-coder-33b-instruct
+#deepseek-ai/deepseek-coder-33b-instruct
 #bigcode/starcoder2-15b-instruct-v0.1
 #bigcode/starcoder2-15b
 #gpt-4-turbo-2024-04-09
-BS=10
-MODEL=gpt-3.5-turbo-0125
+BS=5
 DATASET=wildcodebench
+# MODELS=(Qwen/CodeQwen1.5-7B-Chat)
+# BACKEND=vllm
+MODELS=(gpt-4-turbo-2024-04-09)
 BACKEND=openai
 TEMPS=(0)
 N_SAMPLESES=(1)
 
-for TEMP in ${TEMPS[@]}; do
-  for N_SAMPLES in ${N_SAMPLESES[@]}; do
-    rm -rf $MODEL-$DATASET-$TEMP-$N_SAMPLES.jsonl
-    wildcode.generate \
-        --model $MODEL \
-        --bs $BS \
-        --temperature $TEMP \
-        --n_samples $N_SAMPLES \
-        --resume \
-        --dataset $DATASET \
-        --backend $BACKEND
+for MODEL in ${MODELS[@]}; do
+  for TEMP in ${TEMPS[@]}; do
+    for N_SAMPLES in ${N_SAMPLESES[@]}; do
+      python -m wildcode.generate \
+          --tp 1 \
+          --model $MODEL \
+          --bs $BS \
+          --temperature $TEMP \
+          --n_samples $N_SAMPLES \
+          --resume \
+          --dataset $DATASET \
+          --backend $BACKEND
 
-    # check if "/" in the model name. I
-    if [[ $MODEL == *"/"* ]]; then
-      # split the model name by "/", first part is the organization name, second part is the model name
-      ORG=$(echo $MODEL | cut -d'/' -f1)
-      MODEL=$(echo $MODEL | cut -d'/' -f2)
-      MODEL=$ORG--$MODEL
-    fi
+      if [[ $MODEL == *"/"* ]]; then
+        ORG=$(echo $MODEL | cut -d'/' -f1)
+        MODEL=$(echo $MODEL | cut -d'/' -f2)
+        MODEL=$ORG--$MODEL
+      fi
 
-    SAMPLE_FILE=$MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES.jsonl
-    wildcode.sanitize --samples $SAMPLE_FILE
-    wildcode.evaluate --dataset $DATASET --samples $MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES-sanitized.jsonl
-    wildcode.inspect --dataset $DATASET --eval-results $MODEL-$DATASET-$TEMP-$N_SAMPLES-sanitized_eval_results.json --in-place
+      SAMPLE_FILE=$MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES.jsonl
+      python -m wildcode.sanitize --samples $SAMPLE_FILE
+      rm -rf $MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES-sanitized_eval_results.json
+      python -m wildcode.evaluate --dataset $DATASET --samples $MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES-sanitized.jsonl
+      # python -m wildcode.inspect --dataset $DATASET --eval-results $MODEL--$DATASET--$BACKEND-$TEMP-$N_SAMPLES-sanitized_eval_results.json --in-place
+    done
   done
 done
