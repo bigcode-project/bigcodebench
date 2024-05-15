67d4ddd0dad18f47b4f3b8704b706a5b23a1ecb3
Terry Zhuo <terryzhuo25@gmail.com>
2024-05-15 23:48:43 +0800

    fix: update model range

--- wildcode/generate.py
+++ wildcode/generate.py
@@ -112,7 +112,7 @@ def main():
     
     
     assert args.dataset in ["wildcodebench"], f"Invalid dataset {args.dataset}"
-    assert args.backend in ["vllm", "hf", "openai"]
+    assert args.backend in ["vllm", "hf", "openai", "mistral", "anthropic"]
 
     if args.greedy and (args.temperature != 0 or args.bs != 1 or args.n_samples != 1)\
         or (args.temperature == 0 and args.n_samples == 1):
