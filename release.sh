bd8e4da106987d5756e4eba3120e23342f7d5b1d
Terry Zhuo <terryzhuo25@gmail.com>
2024-06-03 02:46:16 +0800

    fix: split release scripts

--- release.sh
+++ release.sh
@@ -22,22 +22,6 @@ export PYTHONPATH=$PWD pytest tests
 
 git tag $version
 
-docker build
-docker build -f Docker/Evaluate.Dockerfile . -t terryzho/bigcodebench-evaluate:$version
-docker tag terryzho/bigcodebench-evaluate:$version terryzho/bigcodebench-evaluate:latest
-docker push terryzho/bigcodebench-evaluate:$version
-docker push terryzho/bigcodebench-evaluate:latest
-
-docker build -f Docker/Generate_Cuda11.Dockerfile . -t terryzho/bigcodebench-generate-cu11:$version
-docker tag terryzho/bigcodebench-generate-cu11:$version terryzho/bigcodebench-generate-cu11:latest
-docker push terryzho/bigcodebench-generate-cu11:$version
-docker push terryzho/bigcodebench-generate-cu11:latest
-
-docker build -f Docker/Generate_Cuda12.Dockerfile . -t terryzho/bigcodebench-generate-cu12:$version
-docker tag terryzho/bigcodebench-generate-cu12:$version terryzho/bigcodebench-generate-cu12:latest
-docker push terryzho/bigcodebench-generate-cu12:$version
-docker push terryzho/bigcodebench-generate-cu12:latest
-
 rm -rf dist
 python3 -m build
 python3 -m twine upload dist/*
---
+++ release_docker.sh
@@ -0,0 +1,39 @@
+# argument version
+
+set -eux
+
+while getopts "v:" opt; do
+  case $opt in
+    v)
+      version=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ -z "$version" ]; then
+  echo "version is required"
+  exit 1
+fi
+
+export PYTHONPATH=$PWD pytest tests
+
+git tag $version
+
+docker build
+docker build -f Docker/Evaluate.Dockerfile . -t terryzho/bigcodebench-evaluate:$version
+docker tag terryzho/bigcodebench-evaluate:$version terryzho/bigcodebench-evaluate:latest
+docker push terryzho/bigcodebench-evaluate:$version
+docker push terryzho/bigcodebench-evaluate:latest
+
+docker build -f Docker/Generate_Cuda11.Dockerfile . -t terryzho/bigcodebench-generate-cu11:$version
+docker tag terryzho/bigcodebench-generate-cu11:$version terryzho/bigcodebench-generate-cu11:latest
+docker push terryzho/bigcodebench-generate-cu11:$version
+docker push terryzho/bigcodebench-generate-cu11:latest
+
+docker build -f Docker/Generate_Cuda12.Dockerfile . -t terryzho/bigcodebench-generate-cu12:$version
+docker tag terryzho/bigcodebench-generate-cu12:$version terryzho/bigcodebench-generate-cu12:latest
+docker push terryzho/bigcodebench-generate-cu12:$version
+docker push terryzho/bigcodebench-generate-cu12:latest
