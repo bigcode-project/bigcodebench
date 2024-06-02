a41c5872fdb9dfe6122f653ec1b4e06223357749
Terry Zhuo <terryzhuo25@gmail.com>
2024-06-03 02:39:33 +0800

    fix: add generate docker

--- release.sh
+++ release.sh
@@ -22,11 +22,21 @@ export PYTHONPATH=$PWD pytest tests
 
 git tag $version
 
-# docker build
-docker build . -t terryzho/bigcodebench:$version
-docker tag terryzho/bigcodebench:$version terryzho/bigcodebench:latest
-docker push terryzho/bigcodebench:$version
-docker push terryzho/bigcodebench:latest
+docker build
+docker build -f Docker/Evaluate.Dockerfile . -t terryzho/bigcodebench-evaluate:$version
+docker tag terryzho/bigcodebench-evaluate:$version terryzho/bigcodebench-evaluate:latest
+docker push terryzho/bigcodebench-evaluate:$version
+docker push terryzho/bigcodebench-evaluate:latest
+
+docker build -f Docker/Generate_Cuda11.Dockerfile . -t terryzho/bigcodebench-generate-cu11:$version
+docker tag terryzho/bigcodebench-generate-cu11:$version terryzho/bigcodebench-generate-cu11:latest
+docker push terryzho/bigcodebench-generate-cu11:$version
+docker push terryzho/bigcodebench-generate-cu11:latest
+
+docker build -f Docker/Generate_Cuda12.Dockerfile . -t terryzho/bigcodebench-generate-cu12:$version
+docker tag terryzho/bigcodebench-generate-cu12:$version terryzho/bigcodebench-generate-cu12:latest
+docker push terryzho/bigcodebench-generate-cu12:$version
+docker push terryzho/bigcodebench-generate-cu12:latest
 
 rm -rf dist
 python3 -m build
