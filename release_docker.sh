dbf10f0323937de527dc59ec64dda594c6bf1b1c
Terry Zhuo <terryzhuo25@gmail.com>
2024-06-15 02:09:18 +0800

    fix: remove cuda11 release

--- release_docker.sh
+++ release_docker.sh
@@ -20,17 +20,12 @@ fi
 
 export PYTHONPATH=$PWD pytest tests
 
-docker build -f Docker/Evaluate.Dockerfile . -t terryzho/bigcodebench-evaluate:$version
-docker tag terryzho/bigcodebench-evaluate:$version terryzho/bigcodebench-evaluate:latest
-docker push terryzho/bigcodebench-evaluate:$version
-docker push terryzho/bigcodebench-evaluate:latest
+docker build -f Docker/Evaluate.Dockerfile . -t bigcodebench/bigcodebench-evaluate:$version
+docker tag bigcodebench/bigcodebench-evaluate:$version bigcodebench/bigcodebench-evaluate:latest
+docker push bigcodebench/bigcodebench-evaluate:$version
+docker push bigcodebench/bigcodebench-evaluate:latest
 
-docker build -f Docker/Generate_Cuda11.Dockerfile . -t terryzho/bigcodebench-generate-cu11:$version
-docker tag terryzho/bigcodebench-generate-cu11:$version terryzho/bigcodebench-generate-cu11:latest
-docker push terryzho/bigcodebench-generate-cu11:$version
-docker push terryzho/bigcodebench-generate-cu11:latest
-
-docker build -f Docker/Generate_Cuda12.Dockerfile . -t terryzho/bigcodebench-generate-cu12:$version
-docker tag terryzho/bigcodebench-generate-cu12:$version terryzho/bigcodebench-generate-cu12:latest
-docker push terryzho/bigcodebench-generate-cu12:$version
-docker push terryzho/bigcodebench-generate-cu12:latest
+docker build -f Docker/Generate.Dockerfile . -t bigcodebench/bigcodebench-generate:$version
+docker tag bigcodebench/bigcodebench-generate:$version bigcodebench/bigcodebench-generate:latest
+docker push bigcodebench/bigcodebench-generate:$version
+docker push bigcodebench/bigcodebench-generate:latest
