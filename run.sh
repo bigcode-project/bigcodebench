b4d27580c2dd2bcb4a1b84350c0e10ae793b8344
Terry Zhuo <terryzhuo25@gmail.com>
2024-07-16 17:25:40 +0800

    fix: update run example

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ BACKEND=openai
 TEMP=0
 N_SAMPLES=1
 NUM_GPU=1
-SUBSET=instruct
+SPLIT=complete
+SUBSET=hard
 if [[ $MODEL == *"/"* ]]; then
   ORG=$(echo $MODEL | cut -d'/' -f1)--
   BASE_MODEL=$(echo $MODEL | cut -d'/' -f2)
@@ -18,20 +19,21 @@ FILE_HEADER=$ORG$BASE_MODEL--$DATASET-$SUBSET--$BACKEND-$TEMP-$N_SAMPLES
 
 echo $FILE_HEADER
 bigcodebench.generate \
-  --id_range 0 1 \
   --tp $NUM_GPU \
   --model $MODEL \
   --bs $BS \
   --temperature $TEMP \
   --n_samples $N_SAMPLES \
   --resume \
+  --split $SPLIT \
   --subset $SUBSET \
-  --backend $BACKEND
+  --backend $BACKEND \
+  --trust_remote_code
 
 bigcodebench.sanitize --samples $FILE_HEADER.jsonl --calibrate
 
 # Check if the ground truth works on your machine
-bigcodebench.evaluate --subset $SUBSET --samples $FILE_HEADER-sanitized-calibrated.jsonl
+bigcodebench.evaluate --split $SUBSET --samples $FILE_HEADER-sanitized-calibrated.jsonl
 
 # If the execution is slow:
-bigcodebench.evaluate --subset $SUBSET --samples $FILE_HEADER-sanitized-calibrated.jsonl --parallel 32
+bigcodebench.evaluate --split $SUBSET --samples $FILE_HEADER-sanitized-calibrated.jsonl --parallel 32
