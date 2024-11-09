0f4df3e764e9fa132374fbaa206d3caa060219d0
Terry Zhuo <terryzhuo25@gmail.com>
2024-11-09 17:52:44 +0800

    fix(codegen): remove commented code

--- bigcodebench/provider/openai.py
+++ bigcodebench/provider/openai.py
@@ -11,41 +11,7 @@ from bigcodebench.provider.utility import concurrent_call
 class OpenAIChatDecoder(DecoderBase):
     def __init__(self, name: str, base_url=None, **kwargs) -> None:
         super().__init__(name, **kwargs)
-        self.client = openai.OpenAI(
-            api_key=os.getenv("OPENAI_API_KEY", "none"), base_url=base_url
-        )
-
-    # def codegen(
-    #     self, prompts: List[str], do_sample: bool = True, num_samples: int = 200
-    # ) -> List[str]:
-    #     if do_sample:
-    #         assert self.temperature > 0, "Temperature must be positive for sampling"
-    #     all_outputs = []
-    #     for prompt in tqdm(prompts):
-    #         outputs = []
-    #         message = make_raw_chat_prompt(
-    #             task_prompt=prompt,
-    #             subset=self.subset,
-    #             split=self.split,
-    #             instruction_prefix=self.instruction_prefix,
-    #             response_prefix=self.response_prefix,
-    #             tokenizer=None,
-    #         )
-    #         ret = make_auto_request(
-    #             self.client,
-    #             message=message,
-    #             model=self.name,
-    #             max_tokens=self.max_new_tokens,
-    #             temperature=self.temperature,
-    #             n=num_samples,
-    #         )
-    #         for item in ret.choices:
-    #             outputs.append(item.message.content)
-    #         all_outputs.append(outputs)
-    #     return all_outputs
-
-    # def is_direct_completion(self) -> bool:
-    #     return False
+        self.base_url = base_url
     
     def codegen(
         self, prompts: List[str], do_sample: bool = True, num_samples: int = 200
