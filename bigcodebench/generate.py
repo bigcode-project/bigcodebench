e5f27a984ac51bcbe7398ebf275ee498b34ebec0
Terry Zhuo <terryzhuo25@gmail.com>
2024-12-21 17:12:00 +0800

    fix(generate): update the identifier for o1 and o3

--- bigcodebench/generate.py
+++ bigcodebench/generate.py
@@ -189,6 +189,8 @@ def run_codegen(
     )
     
     extra = "-" + subset if subset != "full" else ""
+    if reasoning_effort and model.startswith("o1-") or model.startswith("o3-"):
+        model = model + f"--{reasoning_effort}"
     identifier = model.replace("/", "--") + f"--{revision}--bigcodebench{extra}-{split}--{backend}-{temperature}-{n_samples}-sanitized_calibrated.jsonl"
     
     target_path = os.path.join(root, identifier)
