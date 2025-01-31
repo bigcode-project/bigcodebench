a4f300ab016c1591040c03483da336d8c1154171
Terry Zhuo <terryzhuo25@gmail.com>
2025-01-31 20:15:50 +0800

    fix: remove sample file in example

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ SUBSET=full
 export E2B_API_KEY="e2b_0a231fa3b0a2b01690ab6c66a23b55c0979ce4ee"
 
 bigcodebench.evaluate \
-  --samples bcb_results/gemini-2.0-flash-exp--main--bigcodebench-instruct--google-0-1-sanitized_calibrated.jsonl \
   --model $MODEL \
   --split $SPLIT \
   --subset $SUBSET \
