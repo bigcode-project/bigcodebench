81aca07f26a616ed67960be4f280927b79027eb4
Terry Zhuo <terryzhuo25@gmail.com>
2025-02-14 03:57:26 +0800

    fix passk

--- bigcodebench/evaluate.py
+++ bigcodebench/evaluate.py
@@ -205,7 +205,11 @@ def evaluate(
     else:
         
         pass_at_k = dict()
-        passk = list(pass_k)
+
+        if isinstance(pass_k, str):
+            passk = [int(k) for k in pass_k.split(",")]
+        else:
+            passk = pass_k
         
         if isinstance(selective_evaluate, str):
             selected_ids = set(selective_evaluate.split(","))
