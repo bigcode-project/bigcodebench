9c5726aab90262d91dcafd23daae34bc1598cb03
Terry Zhuo <terryzhuo25@gmail.com>
2025-02-13 21:32:44 +0800

    fix

--- bigcodebench/evaluate.py
+++ bigcodebench/evaluate.py
@@ -210,6 +210,7 @@ def evaluate(
             passk = [int(k) for k in pass_k.split(",")]
         else:
             passk = pass_k
+        
         if isinstance(selective_evaluate, str):
             selected_ids = set(selective_evaluate.split(","))
         else:
