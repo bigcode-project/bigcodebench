9515dfccf0aa1426cf5448e8729c8576bf997b93
Roy Hvaara <roy@lightyear.no>
2025-02-20 18:02:24 +0100

    Move changes to new fix file and bump version numbers

--- tools/fix_v023.py
+++ tools/fix_v023.py
@@ -6,20 +6,52 @@ import copy
 
 BIGCODEBENCH_HF = "bigcode/bigcodebench"
 BIGCODEBENCH_HARD_HF = "bigcode/bigcodebench-hard"
-BIGCODEBENCH_VERSION = "v0.1.2"
+BIGCODEBENCH_VERSION = "v0.1.3"
 BIGCODEBENCH_UPDATE = "bigcode/bcb_update"
-BIGCODEBENCH_NEW_VERSION = "v0.1.3"
+BIGCODEBENCH_NEW_VERSION = "v0.1.4"
 
 def map_ds(sample):
-    if sample["task_id"] in ["BigCodeBench/1005"]:
-        for k in sample.keys():
-            sample[k] = sample[k].replace(
-                "https://getsamplefiles.com/download/zip/sample-2.zip", "https://getsamplefiles.com/download/zip/sample-5.zip"
-            ).replace(
-                "sample_2", "sample_5"
-            ).replace(
-                "Sample 2", "Sample 5"
-            )
+    if sample["task_id"] in ["BigCodeBench/211"]:
+        sample['test'] = sample['test'].replace(
+"""
+        mock_response = MagicMock()
+        mock_response.content = MOCK_CONTENT
+""",
+"""
+        mock_response = MagicMock()
+        mock_response.content = MOCK_CONTENT
+        mock_response.status_code = 200
+"""
+        )
+    if sample["task_id"] in ["BigCodeBench/215"]:
+        sample['test'] = sample['test'].replace(
+"""
+        mock_response = Mock()
+""",
+"""
+        mock_response = Mock()
+        mock_response.status_code = 200
+"""
+        )
+        sample['test'] = sample['test'].replace(
+"""
+        mock_response.text =""",
+"""
+        MOCK_TEXT ="""
+        )
+        sample['test'] = sample['test'].replace(
+"""
+        mock_get.return_value = mock_response
+""",
+"""
+        mock_response.text = MOCK_TEXT
+        mock_response.json = lambda: json.loads(MOCK_TEXT)
+        mock_get.return_value = mock_response
+"""
+        )
+        sample['complete_prompt'] = sample['complete_prompt'].replace("Thif function will raise", "This function will raise")
+        sample['instruct_prompt'] = sample['instruct_prompt'].replace("Thif function will raise", "This function will raise")
+        sample['doc_struct'] = sample['doc_struct'].replace("Thif function will raise", "This function will raise")
     return sample
     
 if __name__ == "__main__":
@@ -28,7 +60,7 @@ if __name__ == "__main__":
     hard_ds_dict = load_dataset(BIGCODEBENCH_HARD_HF)
     ds = ds_dict[BIGCODEBENCH_VERSION]
     hard_ds = hard_ds_dict[BIGCODEBENCH_VERSION]
-    function_id = [1005]
+    function_id = [211, 215]
     
     new_ds = ds.map(map_ds)
     new_ds.to_json("BigCodeBench.jsonl")
